c60f14a67226385fb09cd02c7f5fd9ec0c725bc1
mehdi-aouadi <mehdi.aouadi@gmail.com>
2016-06-08 16:17:58 +0200

    Formatting code

--- OddOccurrencesInArray.cs
+++ OddOccurrencesInArray.cs
@@ -1,12 +1,12 @@
 using System;
 class Solution {
-  public int solution(int[] A) {
-    int result = 0;
+	public int solution(int[] A) {
+	int result = 0;
 		
-		// Using the XOR operator, each pair of duplicated elements will be canceled : A XOR A = 0 and B XOR 0 = B
-    for(int i=0; i<A.Length; i++)
-      result ^= A[i];
-      
-    return result;
-    }
+	// Using the XOR operator, each pair of duplicated elements will be canceled : A XOR A = 0 and B XOR 0 = B
+	for(int i=0; i<A.Length; i++)
+		result ^= A[i];
+		
+	return result;
+	}
 }
