4e86f68ae76940d8d176ae0ddc083966c6ca23e6
mehdi-aouadi <mehdi.aouadi@gmail.com>
2016-06-08 16:17:10 +0200

    Create Odd Occurence Algortithm

---
+++ OddOccurrencesInArray.cs
@@ -0,0 +1,12 @@
+using System;
+class Solution {
+  public int solution(int[] A) {
+    int result = 0;
+		
+		// Using the XOR operator, each pair of duplicated elements will be canceled : A XOR A = 0 and B XOR 0 = B
+    for(int i=0; i<A.Length; i++)
+      result ^= A[i];
+      
+    return result;
+    }
+}
