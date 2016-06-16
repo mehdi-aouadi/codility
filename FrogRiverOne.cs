cbd21b1e1084f81d7871c9bb58cb299fd3183e21
mehdi-aouadi <mehdi.aouadi@gmail.com>
2016-06-16 16:37:31 +0200

    Update code comments

--- FrogRiverOne.cs
+++ FrogRiverOne.cs
@@ -2,23 +2,24 @@ using System;
 
 class Solution {
     public int solution(int X, int[] A) {
-		int[] leaves = new int[X];	//Fallen leaves positions
+    	//Fallen leaves positions
+	int[] leaves = new int[X];
 		
-		//Initializing the leaves positions array
+	//Initializing the leaves positions array
         for(int i=0; i<X; i++)
-			leaves[i] = 0;
-			
+		leaves[i] = 0;
+	
         for(int i=0; i<A.Length; i++)
         {
-			//Memorize the leaves fallen befin the opposite river bank
-            if(A[i]<=X)
-                leaves[A[i]-1]=A[i];	//Memorize the fallen leaf
-            
-			//If the number (i) of fallen leaves is inferiror to the distance (X) no need to check
-			//If there is no more 0 values in the leaves memory array
-			if(i >= X-1 && Array.IndexOf(leaves, 0 ) == -1)
-				return i;
-        }        
+		//Memorize the leaves fallen befin the opposite river bank
+        	if(A[i]<=X)
+                	leaves[A[i]-1]=A[i];	//Memorize the fallen leaf
+                	
+		//If the number (i) of fallen leaves is inferiror to the distance (X) no need to check
+		//If there is no more 0 values in the leaves memory array
+		if(i >= X-1 && Array.IndexOf(leaves, 0 ) == -1)
+			return i;
+        }
         return -1;
 	}
 }
