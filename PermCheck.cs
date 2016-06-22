784ffa50fafb9242e6768f9d22deefe6faa2debf
mehdi-aouadi <mehdi.aouadi@gmail.com>
2016-06-22 10:32:48 +0200

    Renaming res to flags

--- PermCheck.cs
+++ PermCheck.cs
@@ -2,11 +2,11 @@ using System;
 class Solution {
     public int solution(int[] A) {
         int length  = A.Length;
-        int[] res   = new int[length]; //Flag array : 1/0 -> element found/not found
+        int[] flags = new int[length]; //Flag array : 1/0 -> element found/not found
         
         //Initialize the flag array : All elements to 0 (Not yet found)
         for(int i=0; i<length; i++)
-            res[i]=0;
+            flags[i]=0;
             
         for(int i=0; i<length; i++)
         {
@@ -14,14 +14,14 @@ class Solution {
             if(A[i]>length)
                 return 0;
             //If an element is already found
-            if(res[A[i]-1] != 0)
+            if(flags[A[i]-1] != 0)
                 return 0;
             else
-                res[A[i]-1]=1;            
+                flags[A[i]-1]=1;            
         }
         
         //If any element is missing
-        if(Array.IndexOf(res, 0)==-1)
+        if(Array.IndexOf(flags, 0)==-1)
             return 1;
         return 0;        
     }
