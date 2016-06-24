bfd044b7efa20bcb381ae154a870e2fa47ec47a1
mehdi-aouadi <mehdi.aouadi@gmail.com>
2016-06-24 17:45:23 +0200

    Create CountDiv.cs

---
+++ CountDiv.cs
@@ -0,0 +1,9 @@
+using System;
+class Solution {
+    public int solution(int A, int B, int K) {        
+        if(A%K==0)
+            return (B-A)/K+1;
+        else
+            return (B-(A-A%K))/K;
+    }
+}
